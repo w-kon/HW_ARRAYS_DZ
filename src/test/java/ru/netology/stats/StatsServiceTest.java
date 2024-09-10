f321e85f91f9f787664a5271549382269b760fc5
Wlad test <wkon@yandex.ru>
2024-09-10 11:44:45 +0600

    Add test

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -2,81 +2,65 @@ package ru.netology.stats;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import ru.netology.stats.StatsService;
-
-import java.lang.reflect.Array;
 
 
 public class StatsServiceTest {
-    @ParameterizedTest
-//    @CsvFileSource(files = "src/test/resources/DataTest.txt")
+
     long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+
     @Test
-
-    void testSumOfAllSales(long[] sales) {
+    void testSumOfAllSales() {
         StatsService service = new StatsService();
-
-
-
-        // подготавливаем данные:
         long expected = 180;
-
-        // вызываем целевой метод:
         long actual = service.sumOfAllSales(sales);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
 
+
     @Test
-    long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-    void testSumOfAllSales(long[] sales) {
+    void testAverageAmountAllSales() {
         StatsService service = new StatsService();
-
-        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-
-        // подготавливаем данные:
-        long expected = 180;
-
-        // вызываем целевой метод:
-        long actual = service.sumOfAllSales(sales);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
+        long expected = 15;
+        long actual = service.averageAmountAllSales(sales);
         Assertions.assertEquals(expected, actual);
     }
 
+
     @Test
-    void testSumOfAllSales(long[] sales) {
+    void testMonthOfMaximumSales() {
         StatsService service = new StatsService();
-
-        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-
-        // подготавливаем данные:
-        long expected = 180;
-
-        // вызываем целевой метод:
-        long actual = service.sumOfAllSales(sales);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
+        long expected = 8;
+        long actual = service.monthOfMaximumSales(sales);
         Assertions.assertEquals(expected, actual);
     }
 
+
     @Test
-    void testSumOfAllSales(long[] sales) {
+    void testMonthOfMinimumSales() {
         StatsService service = new StatsService();
-
-        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-
-        // подготавливаем данные:
-        long expected = 180;
-
-        // вызываем целевой метод:
-        long actual = service.sumOfAllSales(sales);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
+        long expected = 9;
+        long actual = service.monthOfMinimumSales(sales);
         Assertions.assertEquals(expected, actual);
     }
+
+
+    @Test
+    void testNumberOfMonthsOfSaleIsBelowAverage() {
+        StatsService service = new StatsService();
+        long expected = 5;
+        long actual = service.numberOfMonthsOfSaleIsBelowAverage(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void testNumberOfMonthsOfSaleIsAboveAverage() {
+        StatsService service = new StatsService();
+        long expected = 5;
+        long actual = service.numberOfMonthsOfSaleIsAboveAverage(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
 
